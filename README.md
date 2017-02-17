1d86e6cc96ba9cf84e8b4edaa01bd06d9fcdeeb0
Todd E Thomas <todd.dsm@gmail.com>
2017-02-16 18:04:49 -0600

    1) Added README to describe the process. 2) added env var 'reqdImageId' to debian-8.6-ami.json 3) added env var 'reqdImageId' to debian-8.6-ami.json 4) overhauled build-vm.sh; changed name to 'build-ami.sh' 5) added submodule git@github.com:todd-dsm/aws-tools.git scripts/aws-tools 6) build-ami.sh gets 'reqdImageId' input from: scripts/aws-tools/find-latest-amis.sh

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # process-ph2
-The evolution of a process: phase 2
+The evolution of a process: phase 2.
+
+In Phase 1 an app was developed and sealed into a container. Phase 2 is about making a home for that container in the form of a Amazon AMI. 
+
+Some automation was converted from phase1 to support the environment that will be build in this phase. This will ensure build consistency.
+
+## Some Setup
+Before starting, edit the `vars-build` file in this directory and change the value of `AWS_REGION` from `us-west-2` to whatever region AWS assigned to _you_. 
+
+Also, the work that comes later cannot be validated until there is [access to the VPC]:
+
+* FROM: the point of origin: your current gateway
+* TO: the destination: _your_ AWS account
+
+We may as well get it out of the way now. _NOTE:_ 
+
+* This is only necessary if the gateway address is DHCP (keeps changing) or 
+* You are on the road, staying in hotels for example. 
+* The script only needs to be run once per IP address change. 
+* If the gateway address hasn't changed the script will simply report that the correct settings are already in place. 
+
+Access can be [gained easily] by running this script:
+
+`scripts/aws-tools/access-aws-securitygroup.sh`
+
+Now that the requisite access is in place the AMI can be built.
+***
+## Build the AMI
+Building an AMI means describing it in a JSON file that Packer can consume. In this case that file is `debian-8.6-ami.json`. It needs to be built from an existing AMI so, we'll use the one [Debian] offers in the AWS Marketplace. Since finding the latest AMI `ImageId` requires the manual steps of selecting the **Manual Launch** (tab) and indexing the  `ImageId` in your region the exorcise becomes tedious. We'll get the automation to do it for us.
+
+**WARNING: this step can take 8-12 minutes.**
+
+To build an AMI simply run this script:
+
+`./build-ami.sh debian-8.6-ami.json 2>&1 | tee /tmp/packer-debian-build.out`
+
+### Process Description
+The `build-ami.sh` script takes 1 argument - the packer file: `debian-8.6-ami.json`
+
+After that, `build-ami.sh` calls `scripts/aws-tools/find-latest-amis.sh` to [find the latest] Debian Jessie AMI `ImageId` which is then sent to Packer as a variable; for example: `latestImageId=ami-12345678`. _NOTE: this script does factor for the region specified in_ `vars-build`.
+
+After that, go make a sandwich. It takes a bit to build. When you're ready, review the output in `/tmp/packer-debian-build.out`. These are the build details.
+
+Now that you have a tailored AMI. You are ready to [start Terraforming].
+
+[access to the VPC]:http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-network-security.html#adding-security-group-rule
+[gained easily]:https://github.com/todd-dsm/aws-tools/wiki/access-aws-securitygroup
+[Debian]:https://aws.amazon.com/marketplace/pp/B00WUNJIEE?qid=1487285341617
+[find the latest]:https://github.com/todd-dsm/aws-tools/wiki/find-latest-ami
+[start Terraforming]:https://github.com/todd-dsm/process-ph3
+
---
+++ build-ami.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2086,SC1091
+# QnD scipt to build new Amazon Machine Image.
+#set -x
+
+###----------------------------------------------------------------------------
+### Variables
+###----------------------------------------------------------------------------
+source vars-build
+
+### Determine Latest ImageId
+# assign the result of the script to a variable
+declare reqdImageId="$(eval scripts/aws-tools/find-latest-amis.sh \
+    -r "$AWS_REGION" -n jessie -d debian | tail -1)"
+###----------------------------------------------------------------------------
+declare statusCount='0'
+#export  PACKER_NO_COLOR='false'
+declare packerFile="$1"
+#declare vagrantBox="$HOME/vms/vagrant/boxes/mac_osx/sierra.box"
+#declare isoDir="$HOME/Downloads/isos/osx"
+#declare osxISO='OSX_InstallESD_11.12_16A323.dmg'
+#declare isoURL="$isoDir/$osxISO"
+declare defsValBld="-var latestImageId=$reqdImageId \
+    -var awsRegion=$AWS_REGION \
+    $packerFile"
+
+# Packer may grow to include more testing in the future; form the arrays:
+declare -A subArgs;                declare -a subCMDs;
+subArgs["validate"]="$defsValBld"; subCMDs+=( "validate" )
+subArgs["inspect"]="$packerFile";  subCMDs+=( "inspect" )
+
+
+###----------------------------------------------------------------------------
+### Functions
+###----------------------------------------------------------------------------
+### if a step fails there's no need to go any further
+failMsg()   {
+    echo -e "\n\n    Abort! Abort!    \n\n"
+    exit 1
+}
+
+# Increase statusCount by 1
+passJob() {
+    statusCount="$((statusCount+1))"
+    #printf '%s\n' "  \$statusCount: $statusCount"
+}
+
+# Decrease statusCount by 1
+failJob() {
+    statusCount="$((statusCount-1))"
+    failMsg
+    #printf '%s\n' "  \$statusCount: $statusCount"
+}
+
+checkAMIname() {
+    amiImageId="$1"
+    idLen="$(wc -L <<< ${amiImageId##*-})"     # Typically has a length of 8
+    if [[ "${reqdImageId%-*}" != 'ami' ]]; then
+        printf '\n%s\n' "Verify the AMI ImageId: ${amiImageId}"
+        exit
+    else
+        # match the unique id to a pattern of a flexible length
+        if [[ "${amiImageId##*-}" =~ ^[a-zA-Z0-9_]{"$idLen"}$ ]]; then
+            printf '\n%s\n' "The AMI ImageId looks good: ${amiImageId}"
+        else
+            printf '\n%s\n' "Verify the AMI unique string: ${amiImageId}"
+        fi
+    fi
+}
+
+###----------------------------------------------------------------------------
+### MAIN PROGRAM
+###----------------------------------------------------------------------------
+### Find our current AMI
+###---
+declare currentAMI="$(eval scripts/aws-tools/find-latest-amis.sh \
+    -n base -d self | tail -1)"
+
+# Announce the plan:
+printf '\n%s\n' """
+    The current image $currentAMI is going to be replaced.
+    These are the details:
+    """
+
+# Display AMI details
+aws ec2 describe-images --image-ids "$currentAMI"
+
+
+###---
+### Verify the ImageId fits the pattern
+###---
+checkAMIname "$reqdImageId"
+for i in "${!subCMDs[@]}"; do
+    # Leverage array index to order associative array params/values
+    printf '\n%s\n' "${subCMDs[$i]^}..."
+    # Take advantage of word-splitting; do NOT quote the variables
+    if ! packer ${subCMDs[$i]} ${subArgs[${subCMDs[$i]}]}; then
+        printf '%s\n' "  The packer ${subCMDs[$i]} step failed."
+        failJob
+    else
+        printf '%s\n' "  The packer ${subCMDs[$i]} step passed."
+        passJob
+    fi
+done
+
+
+###---
+### If we made it this far, we're ready; build the VM.
+###---
+if [[ "$statusCount" -ne '2' ]]; then
+    failMsg
+else
+    printf '\n%s\n' "Building the AMI..."
+    # Take advantage of word-splitting; do NOT quote the variable
+    if ! packer build $defsValBld; then
+        printf '\n%s\n' "  The packer build step failed."
+        failMsg
+    fi
+fi
+
+
+###---
+### Display new AMI stats
+###---
+### Find the NEW AMI
+declare newAMI="$(eval scripts/aws-tools/find-latest-amis.sh \
+    -n base -d self | tail -1)"
+
+### Announce the changes:
+printf '\n%s\n' """
+    The packer build step passed.
+    All future builds will be based on the new image: $newAMI
+    These are the details:
+    """
+
+### Display AMI details
+aws ec2 describe-images --image-ids "$newAMI"
+printf '\n%s\n\n' "  Ready for Terraforming!"
+
+
+###---
+### fin~
+###---
+exit 0
