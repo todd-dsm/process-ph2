db2750f15c4b6c0507223fda1e488f7fbd2b6915
Todd E Thomas <todd.dsm@gmail.com>
2017-02-22 09:14:05 -0600

    replaced source_ami with source_ami_filter and remove 21 lines from build-ami.sh

--- build-ami.sh
+++ build-ami.sh
@@ -10,19 +10,13 @@ source vars-build
 
 ### Determine Latest ImageId
 # assign the result of the script to a variable
-declare reqdImageId="$(eval scripts/aws-tools/find-latest-amis.sh \
-    -r "$AWS_REGION" -n jessie -d debian | tail -1)"
+#declare reqdImageId="$(eval scripts/aws-tools/find-latest-amis.sh \
+#    -r "$AWS_REGION" -n jessie -d debian | tail -1)"
 ###----------------------------------------------------------------------------
 declare statusCount='0'
 #export  PACKER_NO_COLOR='false'
 declare packerFile="$1"
-#declare vagrantBox="$HOME/vms/vagrant/boxes/mac_osx/sierra.box"
-#declare isoDir="$HOME/Downloads/isos/osx"
-#declare osxISO='OSX_InstallESD_11.12_16A323.dmg'
-#declare isoURL="$isoDir/$osxISO"
-declare defsValBld="-var latestImageId=$reqdImageId \
-    -var myAWSRegion=$AWS_REGION \
-    $packerFile"
+declare defsValBld="-var myAWSRegion=$AWS_REGION $packerFile"
 
 # Packer may grow to include more testing in the future; form the arrays:
 declare -A subArgs;                declare -a subCMDs;
@@ -52,21 +46,6 @@ failJob() {
     #printf '%s\n' "  \$statusCount: $statusCount"
 }
 
-checkAMIname() {
-    amiImageId="$1"
-    idLen="$(wc -L <<< ${amiImageId##*-})"     # Typically has a length of 8
-    if [[ "${reqdImageId%-*}" != 'ami' ]]; then
-        printf '\n%s\n' "Verify the AMI ImageId: ${amiImageId}"
-        exit
-    else
-        # match the unique id to a pattern of a flexible length
-        if [[ "${amiImageId##*-}" =~ ^[a-zA-Z0-9_]{"$idLen"}$ ]]; then
-            printf '\n%s\n' "The AMI ImageId looks good: ${amiImageId}"
-        else
-            printf '\n%s\n' "Verify the AMI unique string: ${amiImageId}"
-        fi
-    fi
-}
 
 ###----------------------------------------------------------------------------
 ### MAIN PROGRAM
@@ -89,7 +68,6 @@ aws ec2 describe-images --image-ids "$currentAMI"
 ###---
 ### Verify the ImageId fits the pattern
 ###---
-checkAMIname "$reqdImageId"
 for i in "${!subCMDs[@]}"; do
     # Leverage array index to order associative array params/values
     printf '\n%s\n' "${subCMDs[$i]^}..."
@@ -115,27 +93,21 @@ else
     if ! packer build $defsValBld; then
         printf '\n%s\n' "  The packer build step failed."
         failMsg
+    else
+        ### Display new AMI stats
+        declare newAMI="$(eval scripts/aws-tools/find-latest-amis.sh \
+            -n base -d self | tail -1)"
+        ### Announce the changes:
+        printf '\n%s\n' """
+            The packer build step passed.
+            All future builds will be based on the new image: $newAMI
+            These are the details:
+            """
+        ### Display AMI details
+        aws ec2 describe-images --image-ids "$newAMI"
+        printf '\n%s\n\n' "  Ready for Terraforming!"
     fi
 fi
-
-
-###---
-### Display new AMI stats
-###---
-### Find the NEW AMI
-declare newAMI="$(eval scripts/aws-tools/find-latest-amis.sh \
-    -n base -d self | tail -1)"
-
-### Announce the changes:
-printf '\n%s\n' """
-    The packer build step passed.
-    All future builds will be based on the new image: $newAMI
-    These are the details:
-    """
-
-### Display AMI details
-aws ec2 describe-images --image-ids "$newAMI"
-printf '\n%s\n\n' "  Ready for Terraforming!"
 
 
 ###---
